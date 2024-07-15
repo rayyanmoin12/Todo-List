91b3a6d7756384fe0c25bdf5e6be2bedd57629ec
rayyanmoin12 <166857521+rayyanmoin12@users.noreply.github.com>
2024-07-15 21:03:54 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todo :string[] = [];
+
+let while_condition :boolean = true
+
+while (while_condition === true){
+    
+    let ans = await inquirer.prompt([{
+        type: "list",
+        name: "operation",
+        message: "Choose an option",
+        choices: ["Add", "Remove"]
+    }])
+    
+    if (ans.operation === "Add"){
+        let addTask = await inquirer.prompt([{
+            type: "input",
+            name: "add",
+            message: "What woud you like to add?"
+        }])
+        
+        if (addTask.add !== ""){
+            todo.push(addTask.add)
+            console.log(todo)
+        } else {
+            console.log("Please add something!");
+        }
+    }
+    
+    else if (ans.operation === "Remove"){
+        let removeTask = await inquirer.prompt([{
+            type: "list",
+            name: "remove",
+            message: "What would like to remove?",
+            choices: todo
+        }])
+        
+        let index_to_remove = todo.indexOf(removeTask.remove)
+        
+        if (index_to_remove >= 0){
+            todo.splice(index_to_remove, 1)
+            console.log("You removed: ", removeTask.remove);
+            console.log(todo);
+        }
+    }
+    
+    let answer = await inquirer.prompt([{
+        type: "confirm",
+        name: "continue",
+        message: "Do you want to continue?",
+        default: true
+    }])
+    
+    if (answer.continue === false){
+        while_condition = false;
+    }
+    
+}
+
+console.log("Thank you for using Todo list.");
+
+
+
+
